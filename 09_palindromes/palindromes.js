7d011ef9be68f3c3e1f0e9754a37228509f61f5e
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-09-09 11:01:35 +0800

    commit exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let punctuation = /[\.,?!]/g;
+    let newText = word.replace(punctuation, "");
+    newText = newText.toLowerCase()
+    newText = newText.replaceAll(' ', '')
 
+    let splitString = newText.split('')
+    let reversedString = splitString.reverse()
+    let newString = reversedString.join("")
+
+    if (newString === newText) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
