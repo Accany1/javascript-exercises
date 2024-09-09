ac77a57a44678bb6ae46c942b03a3865faa9fdfe
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-09-09 10:50:56 +0800

    commit exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(first,second) {
+	return first + second
 };
 
-const subtract = function() {
-	
+const subtract = function(first,second) {
+	return first - second
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum,current) => sum + current,0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum,current) => sum * current)
 };
 
-const power = function() {
-	
+const power = function(first,second) {
+	return first ** second
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1){
+    return 1
+  }
+
+  for (let i = num - 1; i >= 1; i --) {
+    num = num * i
+  }
+
+  return num
 };
 
 // Do not edit below this line
