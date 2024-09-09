d3af5659ec5958213a8b504538106e9ac94baff3
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-09-09 11:13:08 +0800

    commit exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num)
+    
+    if (num === 0) {
+        return 0
+    } else if (num === 1) {
+        return 1
+    } else if (num < 0) {
+        return "OOPS"
+    }
 
+    let n1 = 0
+    let n2 = 1
+    let nextNum = 0
+
+    for (let i = 1; i < num ; i++){
+        nextNum = n1 + n2
+        n1 = n2
+        n2 = nextNum
+    }
+
+    return nextNum
 };
 
 // Do not edit below this line
