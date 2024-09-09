7fcdba2b7b9690251da834c3d67bd5a1124227cd
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-09-09 11:37:51 +0800

    commit exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = people.map(person => {
+        if (person.yearOfBirth === undefined) {
+            return 0
+        }
+        if (person.yearOfDeath === undefined) {
+            const year = new Date().getFullYear()
+            return year - person.yearOfBirth
+        }
+        return person.yearOfDeath - person.yearOfBirth
+    })
+    const oldestAge = Math.max(...ages)
+    const oldestIndex = ages.indexOf(oldestAge)
+    return people[oldestIndex]
 };
 
 // Do not edit below this line
